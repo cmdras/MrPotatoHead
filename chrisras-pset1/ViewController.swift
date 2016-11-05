8390954fb1df83bf8b9a35e10d87c3c149bf6019
Christopher Ras <chris@MacBook-Pro.local>
2016-11-05 03:16:06 +0100

    Added functionality to most switches.

--- chrisras-pset1/ViewController.swift
+++ chrisras-pset1/ViewController.swift
@@ -20,6 +20,92 @@ class ViewController: UIViewController {
         }
     }
     
+    @IBOutlet weak var eyesSwitch: UISwitch!
+    @IBOutlet weak var eyes: UIImageView!
+    @IBAction func eyesSwitchPressed(_ sender: Any) {
+        if !(eyesSwitch.isOn){
+            eyes.isHidden = true
+        }else{
+            eyes.isHidden = false
+        }
+
+    }
+    
+    @IBOutlet weak var armsSwitch: UISwitch!
+    @IBOutlet weak var arms: UIImageView!
+    @IBAction func armsSwitchPressed(_ sender: Any) {
+        if !(armsSwitch.isOn){
+            arms.isHidden = true
+        }else{
+            arms.isHidden = false
+        }
+
+    }
+    
+    @IBOutlet weak var earsSwitch: UISwitch!
+    @IBOutlet weak var ears: UIImageView!
+    @IBAction func earsSwitchPressed(_ sender: Any) {
+        if !(earsSwitch.isOn){
+            ears.isHidden = true
+        }else{
+            ears.isHidden = false
+        }
+    }
+    
+    @IBOutlet weak var hatSwitch: UISwitch!
+    @IBOutlet weak var hat: UIImageView!
+    @IBAction func hatSwitchPressed(_ sender: Any) {
+        if !(hatSwitch.isOn){
+            hat.isHidden = true
+        }else{
+            hat.isHidden = false
+        }
+
+    }
+    
+    @IBOutlet weak var mouthSwitch: UISwitch!
+    @IBOutlet weak var mouth: UIImageView!
+    @IBAction func mouthSwitchPressed(_ sender: Any) {
+        if !(mouthSwitch.isOn){
+            mouth.isHidden = true
+        }else{
+            mouth.isHidden = false
+        }
+    }
+    
+    @IBOutlet weak var mustacheSwitch: UISwitch!
+    @IBOutlet weak var mustache: UIImageView!
+    @IBAction func mustacheSwitchPressed(_ sender: Any) {
+        if !(mustacheSwitch.isOn){
+            mustache.isHidden = true
+        }else{
+            mustache.isHidden = false
+        }
+    }
+    
+    @IBOutlet weak var noseSwitch: UISwitch!
+    @IBOutlet weak var nose: UIImageView!
+    @IBAction func noseSwitchPressed(_ sender: Any) {
+        if !(noseSwitch.isOn){
+            nose.isHidden = true
+        }else{
+            nose.isHidden = false
+        }
+    }
+    
+    @IBOutlet weak var shoesSwitch: UISwitch!
+    @IBOutlet weak var shoes: UIImageView!
+    @IBAction func shoesSwitchPressed(_ sender: Any) {
+        if !(shoesSwitch.isOn){
+            shoes.isHidden = true
+        }else{
+            shoes.isHidden = false
+        }
+    }
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
